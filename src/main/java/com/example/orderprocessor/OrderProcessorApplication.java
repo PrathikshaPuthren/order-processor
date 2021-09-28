07d05ba868418f982de1435859048d63775f3057
PrathikshaPuthren <Prathiksha.Puthren@target.com>
2021-09-28 08:51:02 +0530

    adding basic auth

--- src/main/java/com/example/orderprocessor/OrderProcessorApplication.java
+++ src/main/java/com/example/orderprocessor/OrderProcessorApplication.java
@@ -3,9 +3,11 @@ package com.example.orderprocessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableWebSecurity
 public class OrderProcessorApplication {
 
   public static void main(String[] args) {
